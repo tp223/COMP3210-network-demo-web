02adef0e3110466ad63a386e6e36787178c6144d
Tom Pavier <tom@electrokid.co.uk>
2024-05-07 16:08:18 +0100

    Unique user setup keys

--- app/Http/Controllers/BeaconSetupController.php
+++ app/Http/Controllers/BeaconSetupController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\BeaconSetup;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BeaconSetupController extends Controller
 {
@@ -12,22 +13,41 @@ class BeaconSetupController extends Controller
      */
     public function register(Request $request)
     {
-        // Validate the request
-        $request->validate([
+        // Validate the API request
+        $validator = Validator::make($request->all(), [
             'serial_number' => 'required|string|max:255',
             'user_key' => 'required|string|max:255',
         ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            return response()->json(['status' => 'error', 'message' => 'Invalid request']);
+        }
+
+
+        // Check if bearer token is provided
+        if ($request->bearerToken()) {
+            // If bearer token already in db then return the beacon setup
+            $beaconSetup = BeaconSetup::where('api_key', $request->bearerToken())->first();
+            if ($beaconSetup) {
+                return response()->json($beaconSetup);
+            }
+        }
 
         // Generate a bearer token
         $bearerToken = substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 60);
 
         // Create a new beacon setup
-        $beaconSetup = new BeaconSetup();
-        $beaconSetup->beacon_id = $request->beacon_id;
-        $beaconSetup->serial_number = $request->serial_number;
-        $beaconSetup->user_key = $request->user_key;
-        $beaconSetup->api_key = $bearerToken;
-        $beaconSetup->save();
+        try {
+            $beaconSetup = new BeaconSetup();
+            $beaconSetup->serial_number = $request->serial_number;
+            $beaconSetup->user_key = $request->user_key;
+            $beaconSetup->api_key = $bearerToken;
+            // Attempt to save the beacon setup
+            $beaconSetup->save();
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => 'Failed to save beacon setup - new token required']);
+        }
 
         // Return the beacon setup
         return response()->json($beaconSetup);
